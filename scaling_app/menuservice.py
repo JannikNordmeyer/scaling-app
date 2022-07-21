48626ccc2ed468b4e86dac6b758baa2bce4c6484
JannikNordmeyer <93387255+JannikNordmeyer@users.noreply.github.com>
2022-07-21 18:44:59 +0200

    feat: Implemented Graphic Display of Lattices.

--- scaling_app/menuservice.py
+++ scaling_app/menuservice.py
@@ -47,7 +47,38 @@ class menuService:
             return
         file = open(filePath)
         data = json.load(file)
-        print(data)
+
+        maxwidth = 1
+        maxdepth = 1
+
+        allnodes = dict()
+
+        for node in data['positions']:
+            for name, coords in node.items():
+                allnodes[name] = coords
+                if abs(coords[0]) > maxwidth:
+                    maxwidth = abs(coords[0])
+                if abs(coords[1]) > maxdepth:
+                    maxdepth = abs(coords[1])
+
+            dc = wx.ClientDC(self.frame.panelLeft)
+
+        width, height =self.frame.panelLeft.GetSize()
+
+        for edges in data['edges']:
+            for origin in edges:
+                for target in edges[origin]:
+                    print(allnodes[origin][0])
+                    dc.DrawLine(allnodes[origin][0]*(width/(2*maxwidth)) + width/2,
+                                allnodes[origin][1]*(height/maxdepth)*0.9 + height*0.04,
+                                allnodes[target][0]*(width/(2*maxwidth)) + width/2,
+                                allnodes[target][1]*(height/maxdepth)*0.9 + height*0.04)
+
+        for node in data['positions']:
+            for name, coords in node.items():
+                dc.DrawCircle(coords[0]*(width/(2*maxwidth)) + width/2, coords[1]*(height/maxdepth)*0.9 + height*0.04, 8)
+                label = wx.StaticText(self.frame.panelLeft, -1, name, (coords[0]*(width/(2*maxwidth)) + width/2 + 10, coords[1]*(height/maxdepth)*0.9 + height*0.04 - 7))
+
 
 
     def saveData(self, e):
